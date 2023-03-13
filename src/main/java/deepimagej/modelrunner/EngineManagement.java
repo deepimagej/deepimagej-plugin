90312dda68b068e654136057d6cdee9969e20846
carlosuc3m <100329787@alumnos.uc3m.es>
2023-03-13 18:38:59 +0100

    add comments

--- src/main/java/deepimagej/modelrunner/EngineManagement.java
+++ src/main/java/deepimagej/modelrunner/EngineManagement.java
@@ -55,13 +55,24 @@ import io.bioimage.modelrunner.engine.EngineInfo;
 import io.bioimage.modelrunner.system.PlatformDetection;
 import io.bioimage.modelrunner.versionmanagement.DeepLearningVersion;
 
+/**
+ * Class that manages the dl-modelrunner engines.
+ * This class checks that the required engines are installed and installs them if they are not.
+ * There is one required engine per DL framework. It can be either the latest one or the one specified
+ * in the variable {@link EngineManagement#ENGINES_VERSIONS}.
+ * This class also contains the methods to install engines on demand.
+ * @author Carlos Garcia Lopez de Haro
+ *
+ */
 public class EngineManagement {
-	
+	/**
+	 * Directory where the engines shold be installed
+	 */
 	private static final String ENGINES_DIR = new File("engines").getAbsolutePath();
-	
+	/**
+	 * Map containing which version should always be installed per framework
+	 */
 	private static final HashMap<String, String> ENGINES_VERSIONS = new HashMap<String, String>();
-	
-	private static final String GENERAL_KEYWORD = "-general";
 	
 	static {
 		ENGINES_VERSIONS.put(EngineInfo.getTensorflowKey() + "_2", "2.7.1");
@@ -69,12 +80,24 @@ public class EngineManagement {
 		ENGINES_VERSIONS.put(EngineInfo.getOnnxKey() + "_1", "1.11.0");
 		ENGINES_VERSIONS.put(EngineInfo.getPytorchKey() + "_1", "1.13.0");
 	}
-	
+	/**
+	 * Key word that substitutes the OS part of teh engine folder name in some
+	 * of the engines installed from the update site
+	 */
+	private static final String GENERAL_KEYWORD = "-general";
+	/**
+	 * Whether the minimum required engines are installed or not
+	 */
 	private boolean everythingInstalled = false;
-	
+	/**
+	 * Which of the required engines are not installed
+	 */
 	private Map<String, String> missingEngineFolders;
 	
-	
+	/**
+	 * Constructor that checks whether the minimum engines are installed
+	 * or not.
+	 */
 	private EngineManagement() {
 		readEnginesJSON();
 		checkEnginesInstalled();
@@ -82,10 +105,17 @@ public class EngineManagement {
 			manageMissingEngines();
 	}
 	
+	/**
+	 * Creates an {@link EngineManagement} object to check if the required engines are installed
+	 * @return
+	 */
 	public static EngineManagement manage() {
 		return new EngineManagement();
 	}
 	
+	/**
+	 * Checks which of the required engines are not installed
+	 */
 	public void checkEnginesInstalled() {
 		Map<String, String> engineFolders = ENGINES_VERSIONS.entrySet().stream()
 				.collect(Collectors.toMap( v -> v.getKey(), v -> {
@@ -108,6 +138,10 @@ public class EngineManagement {
 			everythingInstalled = true;
 	}
 	
+	/**
+	 * Manages the missing engines, either renaming the OS-general engine folder
+	 * to OS-specific or directly installing the engine from scratch
+	 */
 	public void manageMissingEngines() {
 		if (missingEngineFolders == null)
 			checkEnginesInstalled();
@@ -126,6 +160,9 @@ public class EngineManagement {
 		installMissingEngines();
 	}
 	
+	/**
+	 * Install the missing engines from scratch
+	 */
 	public void installMissingEngines() {
 		if (missingEngineFolders == null)
 			checkEnginesInstalled();
@@ -137,7 +174,15 @@ public class EngineManagement {
 		
 	}
 	
-	public boolean installSpecificEngine(String engineDir) {
+	/**
+	 * Install the engine that should be located in the engine dir specified
+	 * @param engineDir
+	 * 	directory where the specific engine shuold be installed. Regard that this 
+	 * 	is the whole path to the folder, and that the folder name should follow the 
+	 * 	dl-modelrunner naming convention (https://github.com/bioimage-io/model-runner-java#readme)
+	 * @return true if the installation was successful and false otherwise
+	 */
+	public static boolean installSpecificEngine(String engineDir) {
 		File engineFileDir = new File(engineDir);
 		if (engineFileDir.mkdirs() == false)
 			return false;
@@ -150,10 +195,28 @@ public class EngineManagement {
 		}
 	}
 	
-	public boolean installEngine(DeepLearningVersion engine) {
+	/**
+	 * Install the engine specified by the {@link DeepLearningVersion} object
+	 * @param engine
+	 * 	the {@link DeepLearningVersion} object specifying the wanted engine
+	 * @return true if the installation was successful and false otherwise
+	 */
+	public static boolean installEngine(DeepLearningVersion engine) {
 		return false;
 	}
 	
+	/**
+	 * Install the engine specified by the arguments of the method
+	 * @param framework
+	 * 	DL framework as specified by the Bioimage.io model zoo ()https://github.com/bioimage-io/spec-bioimage-io/blob/gh-pages/weight_formats_spec_0_4.md)
+	 * @param version
+	 * 	the version of the framework
+	 * @param cpu
+	 * 	whether the engine supports cpu or not
+	 * @param gpu
+	 * 	whether the engine supports gpu or not
+	 * @return true if the installation was successful and false otherwise
+	 */
 	public boolean installEngineForSystemOs(String framework, String version, boolean cpu, boolean gpu) {
 		return false;
 	}
