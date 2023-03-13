c36a2b0d5ec8ed1e716ddbe3b34ac1b063a4f850
carlosuc3m <100329787@alumnos.uc3m.es>
2023-03-13 18:19:06 +0100

    add license

--- src/main/java/deepimagej/modelrunner/EngineManagement.java
+++ src/main/java/deepimagej/modelrunner/EngineManagement.java
@@ -1,3 +1,46 @@
+/*
+ * DeepImageJ
+ * 
+ * https://deepimagej.github.io/deepimagej/
+ * 
+ * Reference: DeepImageJ: A user-friendly environment to run deep learning models in ImageJ
+ * E. Gomez-de-Mariscal, C. Garcia-Lopez-de-Haro, W. Ouyang, L. Donati, M. Unser, E. Lundberg, A. Munoz-Barrutia, D. Sage. 
+ * Submitted 2021.
+ * Bioengineering and Aerospace Engineering Department, Universidad Carlos III de Madrid, Spain
+ * Biomedical Imaging Group, Ecole polytechnique federale de Lausanne (EPFL), Switzerland
+ * Science for Life Laboratory, School of Engineering Sciences in Chemistry, Biotechnology and Health, KTH - Royal Institute of Technology, Sweden
+ * 
+ * Authors: Carlos Garcia-Lopez-de-Haro and Estibaliz Gomez-de-Mariscal
+ *
+ */
+
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2019-2021, DeepImageJ
+ * All rights reserved.
+ *	
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *	  this list of conditions and the following disclaimer in the documentation
+ *	  and/or other materials provided with the distribution.
+ *	
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package deepimagej.modelrunner;
 
 import java.io.File;
@@ -8,15 +51,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.bioimageanalysis.icy.deepicy.tools.CollectionUtils;
-
 import io.bioimage.modelrunner.engine.EngineInfo;
+import io.bioimage.modelrunner.system.PlatformDetection;
+import io.bioimage.modelrunner.versionmanagement.DeepLearningVersion;
 
 public class EngineManagement {
 	
 	private static final String ENGINES_DIR = new File("engines").getAbsolutePath();
 	
 	private static final HashMap<String, String> ENGINES_VERSIONS = new HashMap<String, String>();
+	
+	private static final String GENERAL_KEYWORD = "-general";
 	
 	static {
 		ENGINES_VERSIONS.put(EngineInfo.getTensorflowKey() + "_2", "2.7.1");
@@ -33,8 +78,8 @@ public class EngineManagement {
 	private EngineManagement() {
 		readEnginesJSON();
 		checkEnginesInstalled();
-		if (this.notInstalled)
-			installEngines();
+		if (this.everythingInstalled)
+			manageMissingEngines();
 	}
 	
 	public static EngineManagement manage() {
@@ -61,5 +106,55 @@ public class EngineManagement {
 		
 		if (missingEngineFolders.entrySet().size() == 0)
 			everythingInstalled = true;
+	}
+	
+	public void manageMissingEngines() {
+		if (missingEngineFolders == null)
+			checkEnginesInstalled();
+		if (missingEngineFolders.entrySet().size() == 0)
+			return;
+		missingEngineFolders = missingEngineFolders.entrySet().stream()
+			.filter(v -> {
+				String value = v.getValue();
+				String generalName = value.substring(0, value.indexOf(new PlatformDetection().getOs()));
+				generalName += GENERAL_KEYWORD;
+				File generalFile = new File(generalName);
+				if (generalFile.isDirectory() && generalFile.renameTo(new File(v.getValue())))
+					return false;
+				return true;
+			}).collect(Collectors.toMap(v -> v.getKey(), v -> v.getValue()));
+		installMissingEngines();
+	}
+	
+	public void installMissingEngines() {
+		if (missingEngineFolders == null)
+			checkEnginesInstalled();
+		if (missingEngineFolders.entrySet().size() == 0)
+			return;
+		missingEngineFolders = missingEngineFolders.entrySet().stream()
+				.filter(v -> !installSpecificEngine(v.getValue()))
+				.collect(Collectors.toMap(v -> v.getKey(), v -> v.getValue()));
+		
+	}
+	
+	public boolean installSpecificEngine(String engineDir) {
+		File engineFileDir = new File(engineDir);
+		if (engineFileDir.mkdirs() == false)
+			return false;
+		DeepLearningVersion dlVersion;
+		try {
+			dlVersion = DeepLearningVersion.fromFile(engineFileDir);
+			return installEngine(dlVersion);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public boolean installEngine(DeepLearningVersion engine) {
+		return false;
+	}
+	
+	public boolean installEngineForSystemOs(String framework, String version, boolean cpu, boolean gpu) {
+		return false;
 	}
 }
