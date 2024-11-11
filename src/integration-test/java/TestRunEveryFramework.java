37ada2f140dc92bc8daa78abcf99634ba316f372
carlosuc3m <100329787@alumnos.uc3m.es>
2024-11-11 19:23:42 +0100

    create integration tests instead of unit tests

--- src/integration-test/java/TestRunEveryFramework.java
+++ src/integration-test/java/TestRunEveryFramework.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -16,16 +17,29 @@ import java.util.stream.Collectors;
 import org.junit.Test;
 
 import io.bioimage.modelrunner.bioimageio.BioimageioRepo;
+import io.bioimage.modelrunner.bioimageio.description.ModelDescriptor;
+import io.bioimage.modelrunner.bioimageio.description.ModelDescriptorFactory;
+import io.bioimage.modelrunner.bioimageio.description.exceptions.ModelSpecsException;
 import io.bioimage.modelrunner.bioimageio.download.DownloadModel;
 import io.bioimage.modelrunner.engine.installation.EngineInstall;
 import io.bioimage.modelrunner.engine.installation.FileDownloader;
 import io.bioimage.modelrunner.system.PlatformDetection;
 import io.bioimage.modelrunner.utils.CommonUtils;
+import io.bioimage.modelrunner.utils.Constants;
 import io.bioimage.modelrunner.utils.ZipUtils;
 
 public class TestRunEveryFramework {
 	
 	private List<String> modelPaths;
+	
+	private static final String MACRO_FORMAT = ""
+			+ "run(\"DeepImageJ Run\", "
+			+ "\""
+			+ "modelPath=%s "
+			+ "inputPath=%s "
+			+ "outputFolder=%s "
+			+ "displayOutput=null"
+			+ "\")";
 	
 	private static final File FIJI_DIR = new File("fiji");
 	private static final File ENGINES_DIR = new File(FIJI_DIR.getAbsolutePath(), "engines");
@@ -49,12 +63,17 @@ public class TestRunEveryFramework {
 		FIJI_URL.put(PlatformDetection.OS_OSX, "https://downloads.imagej.net/fiji/latest/fiji-macosx.zip");
 	}
 
-    @Test
-    public void testRun() throws InterruptedException, IOException {
+    @BeforeAll
+    public void setUp() throws InterruptedException, IOException, ModelSpecsException {
     	downloadAndTrackFiji();
     	installEngines();
     	installModels();
-    	//runModels();
+    	compileAndCopyToPlugins();
+    	runModels();
+    }
+    
+    private void compileAndCopyToPlugins() {
+    	// TODO method that executes mvn build in subprocess and copies jar to fiji plugins folder
     }
     
     private static void downloadAndTrackFiji() throws InterruptedException, IOException {
@@ -111,7 +130,17 @@ public class TestRunEveryFramework {
     		String path = br.downloadModelByID(mm, MODELS_DIR.getAbsolutePath());
     		modelPaths.add(path);
     	}
-    	
+    }
+    
+    private void runModels() throws FileNotFoundException, ModelSpecsException, IOException {
+    	for (String mm : modelPaths) {
+    		ModelDescriptor model = ModelDescriptorFactory.readFromLocalFile(mm + File.separator + Constants.RDF_FNAME);
+    		String modelPath = model.getModelPath();
+    		String samplePath = modelPath + File.separator +  model.getInputTensors().get(0).getSampleTensorName();
+    		String macro = String.format(MACRO_FORMAT, modelPath, samplePath, modelPath);
+    		String runModelCommand = "command that runs macros in headless mode";
+    		new ProcessBuilder().command(runModelCommand).start();
+    	}
     }
     
 }
