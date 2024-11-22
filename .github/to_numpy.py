805c92cb3a9a487606148f2f417257a27e318150
carlosuc3m <100329787@alumnos.uc3m.es>
2024-11-23 00:51:00 +0100

    add more things

---
+++ .github/to_numpy.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2024 deepImageJ developers
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""
+Python script that checks that the wanted objects have actually been created
+"""
+
+import json
+import sys
+import os
+import argparse
+
+from io.bioimage.modelrunner.numpy import DecodeNumpy
+from net.imglib2.img.display.imagej import ImageJFunctions
+from ij import IJ
+
+
+for ff in os.listdir("outputs"):
+    print(ff)
+    rai = ImageJFunctions.wrap(IJ.openImage("outputs/" + ff))
+    DecodeNumpy.saveNpy("outputs_npy/" + ff, rai)
+
---
+++ test_samples/test.ijm
@@ -0,0 +1,1 @@
+run("DeepImageJ Run", "modelPath=fiji/Fiji.app/models/brightfield_nuclei_ inputPath=test_image.tif outputFolder=outputs displayOutput=0")
