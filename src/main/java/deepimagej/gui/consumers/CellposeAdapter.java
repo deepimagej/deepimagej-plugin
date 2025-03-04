645fec0e1135c213452f6042c7e92d23355cabfd
carlosuc3m <100329787@alumnos.uc3m.es>
2025-03-04 20:17:19 +0100

    contantly track the changes in focus to be able to have a reactive ui

--- src/main/java/deepimagej/gui/consumers/CellposeAdapter.java
+++ src/main/java/deepimagej/gui/consumers/CellposeAdapter.java
@@ -1,17 +1,43 @@
 package deepimagej.gui.consumers;
 
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.List;
 
 import javax.swing.JComponent;
 
 import ij.ImageListener;
 import ij.ImagePlus;
+import ij.WindowManager;
+import ij.gui.ImageWindow;
 import io.bioimage.modelrunner.gui.ConsumerInterface;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 
 public class CellposeAdapter extends ConsumerInterface implements ImageListener {
+	
+	public CellposeAdapter() {
+		int[] ids = WindowManager.getIDList();
+        if (ids == null) return;
+
+        for (int id : ids) {
+            ImagePlus imp = WindowManager.getImage(id);
+            if (imp == null) continue;
+            ImageWindow win = imp.getWindow();
+            if (win == null) continue;
+            win.addFocusListener(new FocusListener() {
+                @Override
+                public void focusGained(FocusEvent e) {
+                	updateComboBox(imp);
+                }
+                @Override
+                public void focusLost(FocusEvent e) {
+                }
+            });
+            if (win.isFocusOwner()) updateComboBox(imp);
+        }
+	}
 
 	@Override
 	public void setListenersForComponents(List<JComponent> components) {
@@ -37,12 +63,25 @@ public class CellposeAdapter extends ConsumerInterface implements ImageListener 
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private void updateComboBox(ImagePlus imp) {
+		
+	}
 
 	@Override
 	public void imageOpened(ImagePlus imp) {
-		if (!imp.getWindow().isFocused())
-			return;
-		
+        ImageWindow win = imp.getWindow();
+        if (win != null) {
+            win.addFocusListener(new FocusListener() {
+                @Override
+                public void focusGained(FocusEvent e) {
+                    updateComboBox(imp);
+                }
+                @Override
+                public void focusLost(FocusEvent e) {
+                }
+            });
+        }
 	}
 
 	@Override
