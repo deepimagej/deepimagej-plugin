68cfb482aaef56674b21f151a2d0fd104c6e2d7c
carlosuc3m <100329787@alumnos.uc3m.es>
2025-06-06 13:10:17 +0200

    add scripts to test stardist is working on every OS

---
+++ .github/scripts/stardist2d_1c.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2024 deepImageJ developers
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""
+Jython script that downloads Stardist 2d 1 channels and runs it
+"""
+
+from io.bioimage.modelrunner.model.special.stardist import Stardist2D
+import Stardist_DeepImageJ
+
+from ij import IJ
+from net.imglib2.img.display.imagej import ImageJFunctions
+
+import os
+
+
+MODEL_NAME = "StarDist Fluorescence Nuclei Segmentation"
+
+
+model_path = Stardist2D.donwloadPretrained(MODEL_NAME, "") 
+im_path = os.path.join(model_path, "sample_input_0.tif")
+imp = IJ.openImage(im_path)
+rai = ImageJFunctions.wrap(imp)
+
+
+output_rai = Stardist_DeepImageJ.runStarDist(model_path, rai)
+
+max = 0
+cursor = output_rai.cursor()
+while cursor.hasNext():
+	cursor.next()
+	val = cursor.get().getRealDouble()
+	if val > max:
+		max = val
+
+print("Number of instances: " + str(max))
